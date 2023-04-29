194de71b9edcbac5a8f93b4213109bef88a754ff
voncardoso <vonharrison26@gmail.com>
2023-04-29 13:06:12 -0300

    commit initial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,5 +3,5 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1>Hello word</h1>;
+  return <h1>Hello word!</h1>;
 }
